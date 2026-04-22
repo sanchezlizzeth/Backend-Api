3d64064de21b02ac3da333b886e63a1cc51bd20e
Lizzeth Sanchez <sanchezlizzeth328@gmail.com>
2026-04-22 11:45:11 -0600

    Fix middleware API KEY

--- src/server.js
+++ src/server.js
@@ -31,8 +31,10 @@ app.use(cors({
 
 app.use(express.json());
 
-// Middleware API KEY
 app.use((req, res, next) => {
+  if (req.path === "/" || req.path === "/health") {
+    return next();
+  }
   const apiKey = req.headers["x-api-key"];
   if (apiKey !== process.env.API_KEY) {
     return res.status(401).json({ error: "API Key inválida" });
